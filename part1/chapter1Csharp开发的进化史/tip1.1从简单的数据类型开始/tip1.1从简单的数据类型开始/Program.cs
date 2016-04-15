a77fdc70b54ccd29e82fdd84e235a69fb8c67983
yhhno <516061849@qq.com>
2016-04-15 14:06:48 +0800

    init

---
+++ part1/chapter1Csharp开发的进化史/tip1.1从简单的数据类型开始/tip1.1从简单的数据类型开始/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tip1._1从简单的数据类型开始
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ part2/chapter3用泛型实现参数化类型/tip3.1为什么需要泛型/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tip3._1为什么需要泛型
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
